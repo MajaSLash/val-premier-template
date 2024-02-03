aefd2d716d457f034d427a75fdecf5debbb48367
XLRA <milkyscripts@gmail.com>
2024-02-02 22:55:13 -0500

    Basic discord.js bot configuration

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token);
+
